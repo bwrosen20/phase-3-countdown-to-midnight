069cdce7f2046e3233d3e002204bd28513786de8
bwrosen20 <bwrosen20@gmail.com>
2023-06-05 19:51:35 -0400

    commit tree 4a394383832fe73d85c4a0363103b0095cf54612

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
+require "pry"
 
-def countdown
+def countdown(int)
+    i=int
+    while i>0 
+        puts "#{i} SECOND(S)!"
+        i-=1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+    i=int
+    while i>0 
+        puts "#{i} SECOND(S)!"
+        i-=1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
+
+binding.pry
